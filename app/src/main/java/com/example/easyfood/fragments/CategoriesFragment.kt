c85d286333a305a65699bbdbd4a8ec95342c056e
Iva Petkova <iva1petkova@gmail.com>
2022-07-16 17:24:12 +0300

    implement categories list into the categories tab

--- app/src/main/java/com/example/easyfood/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/example/easyfood/fragments/CategoriesFragment.kt
@@ -1,60 +1,69 @@
 package com.example.easyfood.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.easyfood.R
+import com.example.easyfood.activities.CategoryMealsActivity
+import com.example.easyfood.activities.MainActivity
+import com.example.easyfood.adapters.CategoriesAdapter
+import com.example.easyfood.adapters.CategoriesTabAdapter
+import com.example.easyfood.databinding.FragmentCategoriesBinding
+import com.example.easyfood.entities.Category
+import com.example.easyfood.utils.Constants
+import com.example.easyfood.viewModel.HomeViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CategoriesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CategoriesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    private lateinit var binding: FragmentCategoriesBinding
+    private lateinit var categoriesAdapter: CategoriesTabAdapter
+    private lateinit var homeViewModel: HomeViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        homeViewModel = (activity as MainActivity).homeViewModel
+        categoriesAdapter = CategoriesTabAdapter()
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories, container, false)
+    ): View {
+        binding = FragmentCategoriesBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoriesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CategoriesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        prepareCategoriesRecyclerView()
+        homeViewModel.getCategories()
+        observeCategoriesLiveData()
+        onCategoryClick()
+    }
+
+    private fun onCategoryClick() {
+        categoriesAdapter.onItemClick = {
+            val intent = Intent(activity, CategoryMealsActivity::class.java)
+            intent.putExtra(Constants.CATEGORY_NAME, it.strCategory)
+            intent.putExtra(Constants.CATEGORY_DESCRIPTION, it.strCategoryDescription)
+            startActivity(intent)
+        }
+    }
+
+    private fun observeCategoriesLiveData() {
+        homeViewModel.getCategoriesLiveData().observe(viewLifecycleOwner) {
+            categoriesAdapter.setCategories(it as ArrayList<Category>)
+        }
+    }
+
+    private fun prepareCategoriesRecyclerView() {
+        binding.rvCats.apply {
+            layoutManager = GridLayoutManager(context, 1, GridLayoutManager.VERTICAL, false)
+            adapter = categoriesAdapter
+        }
     }
 }
